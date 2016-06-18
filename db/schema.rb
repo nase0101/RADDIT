83f9fa72fcc4e406ccdd78e9d12bfaa24d40b431
Jim <jim@ChendeMacBook-Pro.local>
2016-06-18 21:37:02 +0800

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617134747) do
+ActiveRecord::Schema.define(version: 20160618133157) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
